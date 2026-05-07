19adfe5e33fe9588d98379809ca74d3a6a6a186e
leeminseo <leeminseo0923@gmail.com>
2026-05-07 13:35:44 +0900

    initial commit

---
+++ src/main/kotlin/com/yourssu/ssutimev2/SsutimeV2Application.kt
@@ -0,0 +1,11 @@
+package com.yourssu.ssutimev2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SsutimeV2Application
+
+fun main(args: Array<String>) {
+    runApplication<SsutimeV2Application>(*args)
+}
---
+++ src/test/kotlin/com/yourssu/ssutimev2/SsutimeV2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yourssu.ssutimev2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SsutimeV2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
