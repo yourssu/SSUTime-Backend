c7a249623bcc323cf15b3f87316dfa60c8b588bf
thoorranie <thoorranie1@gmail.com>
2026-09-15 13:34:54 +0900

    Revert "Merge remote-tracking branch 'origin/develop' into feat/24"
    This reverts commit ca793e469128b863eda4ff8f9983b476840f0fa9, reversing
    changes made to 4eee4c4780fae76886fc573dded7f2d07b4bb12f.

--- src/main/kotlin/com/ssutime/auth/infrastructure/UserRepository.kt
+++ src/main/kotlin/com/ssutime/auth/infrastructure/UserRepository.kt
@@ -1,18 +1,8 @@
 package com.ssutime.auth.infrastructure
 
 import com.ssutime.auth.domain.User
-import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Lock
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 
 interface UserRepository : JpaRepository<User, Long> {
     fun findByAuthKey(authKey: String): User?
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select u from User u where u.id = :userId")
-    fun findByIdForBoardReport(
-        @Param("userId") userId: Long,
-    ): User?
 }
--- src/main/kotlin/com/ssutime/notification/domain/event/NewBoardDetected.kt
+++
@@ -1,6 +0,0 @@
-package com.ssutime.notification.domain.event
-
-class NewBoardDetected(
-    val fcmToken: String,
-    val title: String,
-)
